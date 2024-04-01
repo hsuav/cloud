1cbd58a757ace6c714cf235699b96a478bfb0f61
Lee sua <157345690+hsuav@users.noreply.github.com>
2024-04-01 17:19:35 +0900

    Create nat-instance.sh

---
+++ aws/nat-instance.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+yum install -y iptables-services
+systemctl start iptables.services
+systemctl enable iptables.services
+iptables -F
+iptables -t nat -A POSTROUTING -o eh0 -j MASQUERADE
+service iptables save
